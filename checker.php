917f8bfc089883b86eebd34c642b74aa297f63a4
Rares Macovei <69913456+raresmac@users.noreply.github.com>
2023-04-26 18:33:47 +0300

    checker

---
+++ checker.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+//de scos
+$_SESSION['word']=  'elephant';
+$_SESSION['choice']='elehzhnt';
+$_SESSION['letters']=8;
+//pana aici
+
+$result = array();
+$word = str_split($_SESSION['word']);
+$choice = str_split($_SESSION['choice']);
+for($i = 0; $i < $_SESSION['letters']; $i++){
+    if($word[$i] == $choice[$i]){
+        $result[$i] = 2;
+    }
+    else $result[$i] = 0;
+}
+
+for($i = 0; $i < $_SESSION['letters']; $i++){
+    if($result[$i] != 2){
+        for($j = 0; $j < $_SESSION['letters']; $j++){
+            if($i == $j) continue;
+            if($choice[$i] == $word[$j] && $result[$j] != 2){
+                $result[$i] = 1;
+                break;
+            }
+        }
+    }
+}
+?>
